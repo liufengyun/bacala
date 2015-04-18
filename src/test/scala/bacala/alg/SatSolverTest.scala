bb46e232537a00f962a7e24b5fa9a1baab729516
liu fengyun <liufengyunchina@gmail.com>
2015-04-19 00:31:39 +0200

    add missing dependency test case

--- src/test/scala/bacala/alg/SatSolverTest.scala
+++ src/test/scala/bacala/alg/SatSolverTest.scala
@@ -83,4 +83,33 @@ class SatSolverSuite extends BasicSuite {
       DependencyClause(Pack(Root, "2.4.2"), Dep(B, "2.3.4"))
     )))
   }
+
+  object IncompleteRepository extends MiniRepository {
+    val map = Map[PackageT, Set[(Dep, Set[PackageT])]](
+      Pack(Root, "2.4.2") -> Set(
+        Dep(A, "1.3.5") -> Set(Pack(A, "1.3.5")),
+        Dep(B, "2.3.4") -> Set(Pack(B, "2.3.4"))
+      ),
+      Pack(A, "1.3.5") -> Set(
+        Dep(D, "1.1.1") -> Set()
+      ),
+      Pack(B, "2.3.4") -> Set(
+        Dep(C, "2.1.4") -> Set(Pack(C, "2.1.4"))
+      ),
+      Pack(C, "2.1.4") -> Set()
+    )
+
+    override def conflicts = Set(
+      A -> Set(Pack(A, "1.3.5")),
+      B -> Set(Pack(B, "2.3.4")),
+      C -> Set(Pack(C, "2.1.1"), Pack(C, "2.1.4"))
+    )
+  }
+
+  test("report error when there's missing dependency") {
+    assert(new SatSolver(IncompleteRepository).solve === Right(List(
+      DependencyClause(Pack(A, "1.3.5"), Dep(D, "1.1.1")),
+      DependencyClause(Pack(Root, "2.4.2"), Dep(A, "1.3.5"))
+    )))
+  }
 }
