9e04176b3f94f0618bc3c34af1017114d452750c
liu fengyun <liufengyunchina@gmail.com>
2015-04-26 11:13:38 +0200

    Move tree to a separate file

--- src/main/scala/bacala/core/Solver.scala
+++ src/main/scala/bacala/core/Solver.scala
@@ -1,29 +1,5 @@
 package bacala.core
 
-/** Represents a tree structure
-  */
-abstract class Tree[+T, +E] {
-  def value: T
-  def children: List[(E, Tree[T, E])]
-  def +[B >: T, D >: E](edge: D, child: Tree[B, D]): Tree[B, D]
-}
-
-/** Represents non-empty tree
-  */
-case class Node[+T, +E](value: T, children: List[(E, Tree[T, E])] = List[(E, Tree[T, E])]()) extends Tree[T, E] {
-  override def +[B >: T, D >: E](edge: D, child: Tree[B, D]): Tree[B, D] = {
-    Node(value, (edge -> child)::children)
-  }
-}
-
-/** Represents empty leaf node -- it's useful to represent missing dependency or conflict
-  */
-case object Leaf extends Tree[Nothing, Nothing] {
-  override def value = throw new Error("Leaf node")
-  override def children = throw new Error("Leaf node")
-  override def +[B >: Nothing, D >: Nothing](edge: D, child: Tree[B, D]) = throw new Error("Leaf node")
-}
-
 /** Represents resolution result in the dependency graph
   */
 abstract class DependencyEdge[P <: Package, D <: Dependency]
